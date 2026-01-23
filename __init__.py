0ed180e99a459626ab6807b3218f2d9127d7097e
oscarsidsa81 <ovilches@sid-sa.com>
2026-01-23 16:00:17 +0100

    Initial commit

---
+++ __init__.py
@@ -0,0 +1,1 @@
+# módulo solo XML
---
+++ __manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "SID - Sale Order Line Views (OV)",
+    "version": "15.0.1.0.0",
+    "category": "Sales",
+    "summary": "Migración de vistas OV (tree/form/search) de sale.order.line desde Studio/export.",
+    "author": "SID",
+    "license": "LGPL-3",
+    "depends": [
+        "sale",
+        "sale_stock",
+        "stock",
+        "sid_sale_line_custom_fields",
+    ],
+    "data": [
+        "views/sale_order_line_form_readonly_ov.xml",
+        "views/sale_order_line_search_ov.xml",
+        "views/sale_order_line_tree_ov.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
